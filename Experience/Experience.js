068dbf0e8d508f0f460f857c931618072913caf1
ahmadfaqih796 <ahmadfaqih796@gmail.com>
2023-01-02 12:14:09 +0700

    commit

---
+++ Experience/Experience.js
@@ -0,0 +1,14 @@
+import * as THREE from "three";
+
+class Experience {
+  static instance;
+  constructor(canvas) {
+    if (Experience.instance) {
+      return Experience.instance;
+    }
+    Experience.instance = this;
+    this.canvas = canvas;
+  }
+}
+
+export default Experience;
---
+++ main.js
@@ -0,0 +1,6 @@
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
+import { setupCounter } from "./counter.js";
+import Experience from "./Experience/Experience";
+
+const experience = new Experience(document.querySelector(".experience-canvas"));
